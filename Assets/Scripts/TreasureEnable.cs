5fb27b3f0631d7dd808c4a5e79ea145352fb11b6
Irving Rocha <irocha@dreidel.mx>
2020-05-24 02:43:17 -0500

    tutorial

---
+++ Assets/Scripts/TreasureEnable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureEnable : MonoBehaviour
+{
+    public GameObject enableObject;
+    Animator animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = this.gameObject.GetComponent<Animator>();
+        StartCoroutine(WakeUp());
+    }
+
+    IEnumerator WakeUp()
+    {
+        yield return new WaitForSeconds(47.1f);
+        animation.enabled = false;
+        enableObject.active = true;
+    }
+}
